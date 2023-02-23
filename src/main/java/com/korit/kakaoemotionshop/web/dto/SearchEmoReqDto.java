1d5094f0d614a5dfc83d241fee14ae18a07c242b
PK97 <or0.0or2022@gmail.com>
2023-02-23 19:13:33 +0900

    검색결과창 html, SearchEmoReqDto, emo.xml 수정

---
+++ src/main/java/com/korit/kakaoemotionshop/web/dto/SearchEmoReqDto.java
@@ -0,0 +1,25 @@
+package com.korit.kakaoemotionshop.web.dto;
+
+import lombok.Data;
+
+@Data
+public class SearchEmoReqDto {
+
+}
+/*
+
+package com.korit.library.web.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SearchBookReqDto {
+    private int page;
+    private String searchValue;
+    private List<String> categories;
+    private int count;
+    private int userId;
+
+} */
