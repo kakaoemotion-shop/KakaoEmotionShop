0c2525e194cb6b3c553e372c5d0b8f268972c539
PK97 <or0.0or2022@gmail.com>
2023-03-07 02:47:18 +0900

    카카오로 로그인 기능 추가

--- src/main/java/com/korit/kakaoemotionshop/config/SecurityConfig.java
+++ src/main/java/com/korit/kakaoemotionshop/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.korit.kakaoemotionshop.config;
 
+import com.korit.kakaoemotionshop.security.PrincipalOAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +10,16 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpSession;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PrincipalOAuth2DetailsService principalOAuth2DetailsService;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -26,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .atCommonLocations());
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -39,6 +48,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/account/login")
                 .loginProcessingUrl("/account/login")
+                .and()
+                .oauth2Login() //oauth2 로그인 성공 후 가져올 때의 설정들
+                .userInfoEndpoint() // 소셜로그인 성공 시 후속 조치를 진행할 UserService 인터페이스 구현체 등록
+                .userService(principalOAuth2DetailsService) // 리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능 명시
+                .and()
                 .defaultSuccessUrl("/index");
+
     }
 }
--- src/main/java/com/korit/kakaoemotionshop/security/PrincipalDetails.java
+++ src/main/java/com/korit/kakaoemotionshop/security/PrincipalDetails.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -16,7 +17,7 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Getter
     private final UserMst user;
@@ -72,5 +73,15 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return response;
     }
 }
