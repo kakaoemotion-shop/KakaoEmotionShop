01401e19d5f87b2dcd8ea95873f519fa9bdd88d5
hyejin <obndo8539@naver.com>
2023-03-08 16:55:58 +0900

    로그인 에러 문제 해결

--- src/main/java/com/korit/kakaoemotionshop/web/controller/AccountController.java
+++ src/main/java/com/korit/kakaoemotionshop/web/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.korit.kakaoemotionshop.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +12,8 @@ public class AccountController {
     @GetMapping("/login")
     public String login() { return "account/login"; }
 
-    @GetMapping("/login/error")
-    public String loginerror() { return "account/login_error"; }
+    @PostMapping("/login/error")
+    public String loginError() { return "account/login_error"; }
 
     @GetMapping("/register")
     public String register() { return "account/register_sign"; }
