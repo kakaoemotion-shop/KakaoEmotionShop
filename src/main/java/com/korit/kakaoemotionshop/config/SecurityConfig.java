b0498158d21b2d3b98ebcc47dd4b3db0b5ab4515
PK97 <or0.0or2022@gmail.com>
2023-03-14 23:55:40 +0900

    콘피그 수정

--- src/main/java/com/korit/kakaoemotionshop/config/SecurityConfig.java
+++ src/main/java/com/korit/kakaoemotionshop/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .atCommonLocations());
         web.httpFirewall(defaultHttpFirewall());
     }
-
+    
     @Bean
     public HttpFirewall defaultHttpFirewall() {
         return new DefaultHttpFirewall();
